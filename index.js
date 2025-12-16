f372bc163d013283a1c07a145b4305fa6bf533d6
Filipe Cezar <cezarfilipe01@gmail.com>
2025-12-16 07:13:17 -0300

    atualizando
    Aplicando parâmetros nas funções "send"

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get("/blog",function(req,res){
     res.send("Bem vindo ao Meu blog!"); 
 })
 
+app.get("/ola/:nome/:cargo/:cor",function(req,res){
+    res.send("<h1> Olá "+req.params.nome + "</h1>" +  "<h2> Seu cargo é: " + req.params.cargo + "</h2>" + "<h3> Sua Cor favorita é:  " + req.params.cor + "</h2>"); 
+})
+
 app.listen(8081,function(){
     console.log("Servidor Rodando em http://localhost:8081"); 
 }); 
