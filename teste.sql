8165ee471b43eff92bdac8bcbc8c6caa793dcc4a
Filipe Cezar <cezarfilipe01@gmail.com>
2025-12-18 20:58:30 -0300

    atualizando
    Entendendo o Update no SQL

--- teste.sql
+++ teste.sql
@@ -8,3 +8,4 @@ SELECT * FROM usuarios WHERE idade = 18;
 SELECT * FROM usuarios WHERE nome = "Filipe Cezar"; 
 SELECT * FROM usuarios WHERE idade >= 18; 
 DELETE FROM usuarios WHERE nome = "icaro josé"; 
+UPDATE usuarios SET nome = "Nome de Teste" WHERE nome = "Gabriel costa"; 
