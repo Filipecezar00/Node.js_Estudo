23947e5a89f031c92ade510582d1af3cbf43bb40
Filipe Cezar <cezarfilipe01@gmail.com>
2025-12-18 22:07:46 -0300

    atualizando
    Criando bancos e realizando operações simples para controle de dados.

--- sequelize.js
+++ sequelize.js
@@ -1,4 +1,5 @@
 require("dotenv").config(); 
+const { type } = require("os");
 const Sequelize = require("sequelize"); 
 const sequelize = new Sequelize(
     process.env.DB_NAME,
@@ -13,4 +14,36 @@ sequelize.authenticate().then(function(){
     console.log("Conectado, com Sucesso!")
 }).catch(function(erro){
     console.log("Falha ao se conectar"+erro) 
+})
+
+const Postagem = sequelize.define("postagens",{
+    titulo:{
+        type:Sequelize.STRING
+    },
+    conteudo:{
+        type:Sequelize.TEXT 
+    }
+})
+
+
+
+const Usuario = sequelize.define('usuarios',{
+    nome:{
+        type:Sequelize.STRING 
+    }, 
+    sobrenome:{
+        type:Sequelize.STRING
+    }, 
+    idade:{
+        type:Sequelize.INTEGER 
+    }, 
+    email:{
+        type:Sequelize.STRING 
+    }
+})
+Usuario.create({
+    nome:"VICTOR",
+    sobrenome:"vieira", 
+    idade:18, 
+    email:"victor@gmail.com"
 })
