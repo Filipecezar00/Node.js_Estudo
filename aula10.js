7d890b6a6c761ca54faf769ae1b9bdd9403c6db6
Filipe Cezar <cezarfilipe01@gmail.com>
2026-01-05 22:39:10 -0300

    atualizando

--- aula10.js
+++ aula10.js
@@ -4,12 +4,27 @@ const formidable = require("formidable")
 const fs = require('fs')
 
 const servidor = http.createServer((req,res)=>{
+
+    if(req.url=="/envioDeArquivo"){
+        const form = new formidable.IncomingForm(); 
+        form.parse(req,(erro,campos,arquivos)=>{
+            const urlantiga = arquivos.fileupload.path 
+            const urlnova = "C:/Users/User/Desktop/NodeJs/Node.js_Estudo" + arquivos.fileupload[0].name 
+            fs.rename(urlantiga,urlnova,(erro)=>{
+                if(erro) throw erro 
+                res.write('Arquivo Movido!') 
+                res.end()
+            })
+        })
+    }else{
     res.writeHead(200,{'Content-Type':'text/html'})  
     res.write("<form action='envioDeArquivo' method='post' enctype='multipart/form-data'>")
     res.write('<input type="file" name="fileupload"><br> ' )
     res.write("<input type='submit' value='Enviar'>")  
     res.write("</form>") 
     return res.end()
+    }
+    
 }) 
 
 servidor.listen(porta)
