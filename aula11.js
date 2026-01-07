38ac2defdb3d1ee0e420d917f57bc23274de5429
Filipe Cezar <cezarfilipe01@gmail.com>
2026-01-07 10:41:43 -0300

    atualizando

---
+++ aula11.js
@@ -0,0 +1,33 @@
+require("dotenv").config(); 
+const {MongoClient} = require("mongodb") 
+const url = process.env.MONGO_URL 
+const client = new MongoClient(url); 
+
+async function rodar(){
+try{
+    await client.connect() 
+    console.log("Conectado ao Banco de Dados"); 
+
+    const colecao = 'Cursos'; 
+    const dbo = client.db("Cursos")
+    const query = {linguagem:/.t/} 
+
+    const resultado = await dbo.collection(colecao).find(query).toArray()  
+    console.log("Cursos Encontrados"); 
+    console.log(resultado); 
+    
+    
+   
+    // const obj = {curso:"Curso de IA",linguagem:"Phyton"} 
+
+    // const resultado = await dbo.collection(colecao).insertOne(obj) 
+
+    // console.log("1 Novo curso inserido com o ID: " + resultado.insertedId)
+} catch(erro){
+    console.error("Erro na operação: ", erro); 
+}finally{
+    await client.close() 
+}
+}
+
+rodar()
